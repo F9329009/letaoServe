c7dda80c887c12f5c1071733de53d6eae65a7189
F9329009 <9329009@qq.com>
2021-10-23 02:23:13 +0800

    完成：微信支付订单新镇与更新

--- utils/wxPay.js
+++ utils/wxPay.js
@@ -26,7 +26,7 @@ module.exports.wxOrderHandle = async (url, data) => {
 // 生成商户订单号
 module.exports.getTradeNo = (prefix = "") => {
   const date = new Date();
-  return prefix + date.toJSON().substr(0, 11).replace(/[-T]/g, "") + date.getTime() + getRandomByLength(32 - 21 - prefix.length || 0);
+  return prefix + date.toJSON().substr(0, 10).replace(/[-]/g, "") + date.getTime() + getRandomByLength(32 - 21 - prefix.length || 0);
 };
 
 // 生成签名算法
