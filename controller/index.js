03817c08257ef282e40fe0c1369bc6b91e828f34
F9329009 <9329009@qq.com>
2021-10-25 22:02:43 +0800

    修复：补齐首页接口数据

--- controller/index.js
+++ controller/index.js
@@ -1,45 +1,97 @@
 const { query } = require("../utils/mysql");
 // 轮播图
 module.exports.banners = async (ctx, next) => {
+  // const data = await query("SELECT * FROM brand");
   ctx.body = {
     status: 200,
-    swipeList: [],
+    data: [
+      {
+        id: 1,
+        img_src: "/images/banner1.png",
+      },
+      {
+        id: 2,
+        img_src: "/images/banner2.png",
+      },
+      {
+        id: 3,
+        img_src: "/images/banner3.png",
+      },
+      {
+        id: 4,
+        img_src: "/images/banner4.png",
+      },
+      {
+        id: 5,
+        img_src: "/images/banner5.png",
+      },
+    ],
   };
 };
 
 // 运动专区
 module.exports.sports = async (ctx, next) => {
+  // const data = await query("SELECT * FROM category");
   ctx.body = {
     status: 200,
-    sportsList: [],
+    data: [
+      {
+        name: "adidas阿迪达斯 男式 场下休闲篮球鞋S83700",
+        img: "/images/product.jpg",
+        price: 1.0,
+        oldPrice: 888.0,
+      },
+      {
+        name: "FORUM 84 LOW 新款低帮经典运动鞋",
+        img: "/images/product.jpg",
+        price: 1.0,
+        oldPrice: 899.0,
+      },
+      {
+        name: "adidas阿迪达斯 男式 场下休闲篮球鞋S83700",
+        img: "/images/product.jpg",
+        price: 1.0,
+        oldPrice: 888.0,
+      },
+      {
+        name: "adidas阿迪达斯 男式 场下休闲篮球鞋S83700",
+        img: "/images/product.jpg",
+        price: 1.0,
+        oldPrice: 888.0,
+      },
+    ],
   };
 };
 
 // 宫格列表
 module.exports.gridlist = async (ctx, next) => {
-  query("SELECT * FROM category");
+  // const data = await query("SELECT * FROM brand");
   ctx.body = {
     status: 200,
-    gridlist: [
+    data: [
       {
         id: 1,
-        img: "/images/nav1.png",
+        img_src: "/images/nav1.png",
       },
       {
         id: 2,
-        img: "/images/nav2.png",
+        img_src: "/images/nav2.png",
       },
       {
         id: 3,
-        img: "/images/nav3.png",
+        img_src: "/images/nav3.png",
       },
       {
         id: 4,
-        img: "/images/nav4.png",
+        img_src: "/images/nav4.png",
       },
       {
         id: 5,
-        img: "/images/nav5.png",
+        img_src: "/images/nav5.png",
+      },
+      {
+        id: 6,
+        img_src: "/images/nav6.png",
       },
     ],
   };
